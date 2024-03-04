4a0c9c3e9c7159dec7eaa77ceeabe4ae7a7accd7
Amira Ezzeldin <aezz4007@gmail.com>
2024-03-04 05:07:55 +0200

    Generate a random computer choice

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-console.log('This is the basic JavaScript file');
+// A generator of a random number < max
+function random(max) {
+    return Math.floor(Math.random() * max);
+}
+
+// Generate a random computer choice
+function getComputerChoice () {
+    const CHOICES = ['rock', 'paper', 'scissors'];
+    return CHOICES[random(CHOICES.length)];
+}
