23bad1ff00f7300cdb1293cd74dce2525e4a98de
Amira Ezzeldin <aezz4007@gmail.com>
2024-03-04 07:07:10 +0200

    Create a function to get user's choice

--- scripts.js
+++ scripts.js
@@ -1,10 +1,27 @@
+// A global array of choices
+const CHOICES = ['rock', 'paper', 'scissors'];
+
 // A generator of a random number < max
 function random(max) {
     return Math.floor(Math.random() * max);
 }
 
 // Generate a random computer choice
-function getComputerChoice () {
-    const CHOICES = ['rock', 'paper', 'scissors'];
+function getComputerChoice() {
     return CHOICES[random(CHOICES.length)];
 }
+
+// Get player choice
+function getPlayerChoice() {
+    let entry = prompt(`Please enter your choice: 'rock', 'paper' or 'scissors'`);
+    if (entry === null) {
+        console.log('Game aborted by user');
+        return;
+    } else if (!CHOICES.includes(entry)) {
+        return entry = getPlayerChoice();
+    } else {
+        return entry.toLowerCase();
+}
+}
+
+console.log(getPlayerChoice());
