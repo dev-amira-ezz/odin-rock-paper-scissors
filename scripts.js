2f07d9905d1e77ad424216bea868b8f61624a2a0
Amira Ezzeldin <aezz4007@gmail.com>
2024-03-05 02:05:29 +0200

    Fix: handled reduntant function calls

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
-// A global array of choices
+// An array of choices
 const getChoices = () => ['rock', 'paper', 'scissors'];
-
 
 // A generator of a random number < max
 const random = (max) => Math.floor(Math.random() * max);
 
 // Generate a random computer choice
-const getComputerChoice = () => getChoices()[random(getChoices().length)];
+const getComputerChoice = () => {
+    const choices = getChoices();
+    return choices[random(choices.length)];
+}
 
 // A prompt to get user entry
 const getEntry = () => prompt(`Please enter your choice: 'rock', 'paper' or 'scissors'`);
@@ -15,7 +17,7 @@ const getEntry = () => prompt(`Please enter your choice: 'rock', 'paper' or 'sci
 const getPlayerChoice = () => {
     const entry = getEntry();
     return entry === null
-        ? console.log('Game aborted by user!')
+        ? (console.log('Game aborted by user!'), null)
         : !getChoices().includes(entry.toLowerCase())
             ? getPlayerChoice()
             : entry.toLowerCase();
