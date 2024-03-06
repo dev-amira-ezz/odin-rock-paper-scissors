cf0f30c5d4f0bd14833946d34727d0dabf8b25bd
Amira Ezzeldin <aezz4007@gmail.com>
2024-03-06 20:14:18 +0200

    Separate playGame function into smaller functions

--- scripts.js
+++ scripts.js
@@ -1,97 +1,87 @@
-// An array of choices
 const getChoices = () => ['rock', 'paper', 'scissors'];
 
-// A generator of a random number < max
 const random = (max) => Math.floor(Math.random() * max);
 
-// Generate a random computer choice
 const getComputerChoice = () => {
     const choices = getChoices();
     return choices[random(choices.length)];
 }
 
-// A prompt to get user entry
 const getEntry = () => prompt(
     `Please enter your choice: 'rock', 'paper' or 'scissors'`
 );
 
-// Get player choice
+// handle user cancellation and wrong entries
 const getPlayerChoice = () => {
     const entry = getEntry();
-    // In case of a player hits cancel
     return entry === null
         ? (console.log('Game aborted by user!'), null)
-        // In case the player enters something other than game choices
         : !getChoices().includes(entry.toLowerCase())
-            // Call this function again to get a correct player choice
             ? getPlayerChoice()
-            // Returns user's entry in lower case in case of correct choice
             : entry.toLowerCase();
 }
 
-// play a round of the game
+// Play a round. If it's a tie, ask for another user entry
 const playRound = (playerSelection, computerSelection) => {
     if (playerSelection === null) {
         return;
-    }
-    // If it's not a tie, play a round of the game
-    else if (playerSelection !== computerSelection) {
+    } else if (playerSelection !== computerSelection) {
         return getWinner(playerSelection, computerSelection);
     } else {
         console.log(`It's a tie! You both chose ${playerSelection}`);
-        // Call this function again until the tie breaks
         return playRound(getPlayerChoice(), getComputerChoice());
     }
 }
 
-// win conditions to check in the if statement of getWinner function
+// Conditions used by the if statement to determine the winner
 const winConditions = (playerSelection, computerSelection) => (
     (playerSelection === 'rock' && computerSelection === 'scissors') ||
     (playerSelection === 'paper' && computerSelection === 'rock') ||
     (playerSelection === 'scissors' && computerSelection === 'paper')
-)
+);
 
-// A round of the game
 const getWinner = (playerSelection, computerSelection) => {
     if (winConditions(playerSelection, computerSelection)) {
-        // Handle winning
         console.log(`You win! ${playerSelection} beats ${computerSelection}`);
         return 1;
     } else {
-        // Handle losing
         console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
         return 0;
     }
 }
 
-// Play a game of five rounds
-const playGame = (playerSelection, computerSelection) => {
-    let numberOfRounds = 0;
-    let score = 0;
-    for (let i = 0; i < 5; i++) {
-        // Check returned value from playRound function
-        if (playerSelection === null) {
-            return;
-        }
-        const result = playRound(playerSelection, computerSelection);
-        if (result == 1) {
-            score++;
-            console.log(`Your score is ${score}`);
-        } else {
-            console.log(`Your score is ${score}`)
-        }
-        numberOfRounds++;
-        console.log(`Number of rounds played = ${numberOfRounds}`);
-        // Reset player and computer choices
-        playerSelection = getPlayerChoice();
-        computerSelection = getComputerChoice();
-    }
+const showFinalScore = (score) => {
     console.log(`Game over! Your total score is ${score}/5`);
-    if (score >=3) {
+    if (score >= 3) {
         console.log('You won the game');
     } else {
         console.log('You lost the game!');
     }
 }
-// Call the playGame function
+
+const showRoundDetails = (score, numberOfRounds) => {
+    console.log(`Your score is ${score}`);
+    console.log(`Number of rounds played = ${numberOfRounds}`);
+}
+
+const playGame = (playerSelection, computerSelection) => {
+    let numberOfRounds = 0;
+    let score = 0;
+    for (let i = 0; i < 5; i++) {
+        if (playerSelection === null) {
+            return;
+        }
+        // In case of the player wins the round
+        const result = playRound(playerSelection, computerSelection);
+        if (result == 1) {
+            score++;
+        }
+        numberOfRounds++;
+        showRoundDetails(score, numberOfRounds);
+        // Reset data to start new round with fresh data
+        playerSelection = getPlayerChoice();
+        computerSelection = getComputerChoice();
+    }
+    showFinalScore(score);
+}
 playGame(getPlayerChoice(), getComputerChoice());
